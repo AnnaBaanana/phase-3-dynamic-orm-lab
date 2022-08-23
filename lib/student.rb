f365b7378d112b9558e1bacf18d902fb145a1d27
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-08-23 14:31:57 -0400

    commit tree 76651b14953873a53c8b4c2537b60eb2a008391f

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,10 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require "pry"
 
 class Student < InteractiveRecord
 
 end
+
+binding.pry
